1658f83e03bc1ad2cf5a3f19e0dde3e591f37e41
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2019-12-17 19:26:11 +0530

    added service worker build 9

--- src/index.js
+++ src/index.js
@@ -11,4 +11,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register({ onUpdate: () => {
     console.log('+++++++++++++++++++++++++++++++++++++++++++++++ new build is deployed');
+    window.alert('please Refresh the page');
 } });
