839d3fea654ce8dc7b56e385276c96b2cebe21c1
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2019-12-17 20:12:23 +0530

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MiniDrawer from './drawerLayout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          this is my first deployed app new build 3
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MiniDrawer />
   );
 }
 
