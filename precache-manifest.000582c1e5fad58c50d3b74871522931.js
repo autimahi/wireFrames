4c630f3a72273dc84cd5bb530ce8cbd210085846
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2019-12-17 18:08:39 +0530

    Updates

---
+++ precache-manifest.000582c1e5fad58c50d3b74871522931.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a5626d564b5e069234d24292a7a453c",
+    "url": "/wireFrames/index.html"
+  },
+  {
+    "revision": "2fca60429a3566303cb6",
+    "url": "/wireFrames/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bbdffde685f56cee2645",
+    "url": "/wireFrames/static/js/2.2a595436.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wireFrames/static/js/2.2a595436.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fca60429a3566303cb6",
+    "url": "/wireFrames/static/js/main.b88bbd9f.chunk.js"
+  },
+  {
+    "revision": "295421ecc57e58de4eb4",
+    "url": "/wireFrames/static/js/runtime-main.5a9dc8b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wireFrames/static/media/logo.5d5d9eef.svg"
+  }
+]);
